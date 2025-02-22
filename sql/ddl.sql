ea47c41a85489895633d97bc160b05882503ecd3
haloapping <alfiyanto.kondolele@gmail.com>
2025-02-22 17:14:03 +0700

    fix: refine details

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,8 +1,7 @@
 CREATE TABLE fishes (
 	id UUID PRIMARY KEY,
-	name VARCHAR NOT NULL UNIQUE,
-	scientific_name VARCHAR DEFAULT NULL UNIQUE,
-	habitat VARCHAR DEFAULT NULL,
+	name VARCHAR NOT NULL,
+	scientific_name VARCHAR DEFAULT NULL,
 	size VARCHAR DEFAULT NULL,
 	diet VARCHAR DEFAULT NULL,
 	lifespan VARCHAR DEFAULT NULL,
@@ -11,7 +10,7 @@ CREATE TABLE fishes (
 	water_type VARCHAR DEFAULT NULL,
 	reproduction VARCHAR DEFAULT NULL,
 	behavior VARCHAR DEFAULT NULL,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	created_at TIMESTAMP DEFAULT NOW(),
 	updated_at TIMESTAMP DEFAULT NULL
 );
 
@@ -19,8 +18,8 @@ CREATE INDEX idx_fishes ON fishes (id, name);
 
 CREATE TABLE predators (
 	id UUID PRIMARY KEY,
-	name VARCHAR NOT NULL UNIQUE,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	name VARCHAR NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
 	updated_at TIMESTAMP DEFAULT NULL
 );
 
@@ -39,8 +38,8 @@ CREATE TYPE habitat_enum AS ENUM (
 
 CREATE TABLE habitats (
 	id UUID PRIMARY KEY,
-	name habitat_enum NOT NULL UNIQUE,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	name habitat_enum NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
 	updated_at TIMESTAMP DEFAULT NULL
 );
 
@@ -50,7 +49,7 @@ CREATE TABLE fishes_habitats (
 	id UUID PRIMARY KEY,
 	fish_id UUID NOT NULL,
 	habitat_id UUID NOT NULL,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	created_at TIMESTAMP DEFAULT NOW(),
 	updated_at TIMESTAMP DEFAULT NULL,
 	FOREIGN KEY(fish_id) REFERENCES fishes(id) ON DELETE CASCADE,
 	FOREIGN KEY(habitat_id) REFERENCES habitats(id)  ON DELETE CASCADE
@@ -60,11 +59,21 @@ CREATE TABLE fishes_predators (
 	id UUID PRIMARY KEY,
 	fish_id UUID NOT NULL,
 	predator_id UUID NOT NULL,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	created_at TIMESTAMP DEFAULT NOW(),
 	updated_at TIMESTAMP DEFAULT NULL,
 	FOREIGN KEY(fish_id) REFERENCES fishes(id) ON DELETE CASCADE,
 	FOREIGN KEY(predator_id) REFERENCES predators(id) ON DELETE CASCADE
 );
 
-DROP TABLE fishes_habitats, fishes_predators, fishes, predators;
-DROP TABLE habitats;
+DROP TABLE fishes_habitats, fishes_predators, fishes, predators, habitats;
+DROP TYPE habitat_enum;
+
+DELETE FROM fishes_habitats;
+DELETE FROM fishes_predators;
+DELETE FROM fishes;
+DELETE FROM habitats;
+DELETE FROM predators;
+
+
+
+
