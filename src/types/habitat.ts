b8bb6ce21a2978c5a35579ad02430a4cd147e26c
haloapping <alfiyanto.kondolele@gmail.com>
2025-03-11 20:15:54 +0700

    fix: change type of slug field to string

--- src/types/habitat.ts
+++ src/types/habitat.ts
@@ -3,16 +3,7 @@ import { z } from "zod";
 export const HabitatSchema = z.object({
   id: z.string().optional(),
   slug: z.string().min(1).optional(),
-  name: z.enum([
-    "Freshwater",
-    "Saltwater",
-    "Brackish",
-    "River",
-    "Lake",
-    "Deep Sea",
-    "Reef",
-    "Open Ocean",
-  ]),
+  name: z.string().min(1),
   createdAt: z.date().optional().optional(),
   updatedAt: z.union([z.date(), z.null()]).optional(),
 });
